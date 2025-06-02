95e29928326453c1c57c7f8218669900e30d3538
Murage Kibicho <murage.kibicho@gmail.com>
2025-06-02 13:51:47 +0300

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,15 +2,15 @@ import express, { Router } from "express";
 import serverless from "serverless-http";
 import bodyParser from "body-parser";
 import compression from "compression";
-
 const app = express();
 const router = Router();
+const __dirname = import.meta.dirname;
 
 app.use(bodyParser.json());
 app.use(compression());
 
 router.get("/hello", (req, res) => res.send("Hello World!"));
-router.get("/canvas", (req, res) => res.sendFile('CanvasPage/index.html'));
+router.get("/canvas", (req, res) => res.sendFile('CanvasPage/index.html',{ root: __dirname }));
 app.use("/", router);
 
 export const handler = serverless(app);
