c56f5443f7476fa43090e52604fa263a8be76ae1
Murage Kibicho <murage.kibicho@gmail.com>
2025-06-02 13:19:08 +0300

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -9,6 +9,6 @@ app.use(bodyParser.json());
 
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
-app.use("/", router);
+app.use("/home", router);
 
 export const handler = serverless(app);
