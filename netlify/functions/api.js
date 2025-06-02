4ef761140054c9d54d7816204dc88af920f0ee48
Murage Kibicho <murage.kibicho@gmail.com>
2025-06-02 13:26:13 +0300

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,11 +1,13 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
 import bodyParser from "body-parser";
+import compression from "compression";
 
 const app = express();
 const router = Router();
 
 app.use(bodyParser.json());
+app.use(compression());
 
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
